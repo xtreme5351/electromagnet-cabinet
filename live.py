d28b14614204347ce78238ad126f9a4d737b5463
xtreme5351 <45394613+xtreme5351@users.noreply.github.com>
2020-06-18 19:05:55 +0100

    version 1.0.0
    Now it fully works, added timers to keep track of suspension and how long the script can run for. Also added a method to keep track fo the total number of recognised or failed faces, in order to make sure the class can be called and will either return True (if the face is recognised for 6 conintous frames) or return False (if no face is detected after 2 consecutive suspension cycles)

--- live.py
+++ live.py
@@ -13,12 +13,13 @@ from imutils.video import FPS
 
 pickledData = r"D:\PycharmProjects\python\machineLearning\ElectromagnetCabinet\dataset\faceEncoding.pkl"
 model = "hog"
+version = "1.0.0"
 
 class liveRecognition():
-
     def recognise(orgEncoding, inpEncoding, boxes, frame):
         names = ["boi"]
         name = 'boi'
+
         print(type(orgEncoding))
         print(type(inpEncoding))
         data = pickle.loads(open(orgEncoding, "rb").read())
@@ -43,61 +44,111 @@ class liveRecognition():
                 check += 1
 
         confidence = (check / len(temp)) * 100
-        print("Positive Percentage: ", confidence, "%")
+        print("Confidence: ", confidence, "%")
 
         if confidence > 60.000:
             for ((top, right, bottom, left), name) in zip(boxes, names):
                 cv2.rectangle(frame, (left, top), (right, bottom), (150, 150, 0), 2)
                 cv2.putText(frame, name, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 1)
+            liveRecognition.stasis("rf", 0)
+            global detected
+            detected += 1
         else:
-            pass
+            for ((top, right, bottom, left), name) in zip(boxes, names):
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 0), 2)
+            liveRecognition.stasis("f", 0)
 
 
 
-    def load(frame, mod):
+    def stasis(state, t):
+        print("===", state, "===")
+        if state == "rf":
+            print("Face recognised, continuing")
+
+        elif state == "f":
+            print("Face detected, still continuing")
+
+        elif state == "nrf":
+            t2 = time.perf_counter()
+            print("No face detected, beginning stasis")
+            elapsed = t - t2
+            print("{} seconds since last face detected".format(round(elapsed, 4)))
+            liveRecognition.recogStart(1)
+            
+        
+
+    def load(frame, mod, t):
         print("Encoding frame")
-
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         boxes = face_recognition.face_locations(rgb, model=mod)
         face = face_recognition.face_encodings(rgb, boxes)
         print("=== FACE ===: ", face)
         print("FACE ARR LENGTH: ", len(face))
-        global returned
-        if not face:
-            liveRecognition.recogStart(1)
-        else: 
-            returned = face[0]
+       
+        t1 = time.perf_counter()
+        print(f"Execution time {t1 - t:0.4f}s")
+        global failed
 
-        liveRecognition.recognise(pickledData, returned, boxes, frame)
+        if failed > 30:
+            print("=== Suspending ===")
+            global totalSuspension
+            totalSuspension += 1
+            time.sleep(60)
+            failed = 1
+            print("=== Resuming ===")
+
+        if face == []:
+            print("No. of frames with no face: ", failed)
+            failed += 1
+            liveRecognition.recogStart(1) 
+        else: 
+            failed = 1
+            liveRecognition.recognise(pickledData, face[0], boxes, frame)
        
 
 
     def recogStart(a):
         while True:
+            print("\nCONFIRMED FACES = ", detected)
+            print("FAILED FACES = ", failed)
+
+            if detected > 5:
+                return True
+                break
+            
+            if totalSuspension > 1:
+                return False
+                break
+            
             frame = video.read()
             if a == 0:
                 frame = imutils.resize(frame, width=1020)
-                liveRecognition.load(frame, model)
+                liveRecognition.load(frame, model, 0)
                 cv2.imshow("Video", frame)
                 key = cv2.waitKey(1) & 0xFF
                 if key == ord("k"):
                     break
-
-                time.sleep(2.0)
-                # cv2.destroyWindow("Video")
 
             elif a == 1:
-                liveRecognition.load(frame, model)
+                t1 = time.perf_counter()
+                frame = imutils.resize(frame, width=1020)
+                liveRecognition.load(frame, model, t1)
                 cv2.imshow("Video", frame)
                 key = cv2.waitKey(1) & 0xFF
                 if key == ord("k"):
                     break
 
-                time.sleep(2.0)
 
+    def main(self):
+        global failed
+        failed = 1
 
+        global detected
+        detected = 0
 
-    def __init__(self):
+        global totalSuspension
+        totalSuspension = 0
+
         print("Starting video...")
         global video
         video = VideoStream().start()
@@ -106,6 +157,12 @@ class liveRecognition():
             raise IOError("CANNOT START VIDEO")
 
         time.sleep(1.0)
-        liveRecognition.recogStart(0)
+        result = liveRecognition.recogStart(0)
 
-liveRecognition()
+        if result == True:
+            return True
+        elif result == False:
+            return False
+
+res = liveRecognition().main()
+print(res)
